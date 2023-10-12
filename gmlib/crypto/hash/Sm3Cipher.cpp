667aa6e853f063a49e3b4a94cb6c82eb3876b9ac
oldprincess <zirui.gong@foxmail.com>
2023-10-12 23:19:08 +0800

    feature: optimize sm3 cipher

--- gmlib/crypto/hash/Sm3Cipher.cpp
+++ gmlib/crypto/hash/Sm3Cipher.cpp
@@ -36,177 +36,189 @@ using Sm3CTX = gmlib::Sm3Cipher::Sm3CTX;
 
 #pragma region "SM3 Core Algorithm"
 
-#define MEM_LOAD32BE(src)                        \
-    (((uint32_t)(((uint8_t *)(src))[0]) << 24) | \
-     ((uint32_t)(((uint8_t *)(src))[1]) << 16) | \
-     ((uint32_t)(((uint8_t *)(src))[2]) << 8) |  \
-     ((uint32_t)(((uint8_t *)(src))[3]) << 0))
+#define MEM_LOAD32BE(src)                       \
+    (((uint32_t)(((uint8_t*)(src))[0]) << 24) | \
+     ((uint32_t)(((uint8_t*)(src))[1]) << 16) | \
+     ((uint32_t)(((uint8_t*)(src))[2]) << 8) |  \
+     ((uint32_t)(((uint8_t*)(src))[3]) << 0))
 
-#define MEM_STORE32BE(dst, a)                              \
-    (((uint8_t *)(dst))[0] = ((uint32_t)(a) >> 24) & 0xFF, \
-     ((uint8_t *)(dst))[1] = ((uint32_t)(a) >> 16) & 0xFF, \
-     ((uint8_t *)(dst))[2] = ((uint32_t)(a) >> 8) & 0xFF,  \
-     ((uint8_t *)(dst))[3] = ((uint32_t)(a) >> 0) & 0xFF)
+#define MEM_STORE32BE(dst, a)                             \
+    (((uint8_t*)(dst))[0] = ((uint32_t)(a) >> 24) & 0xFF, \
+     ((uint8_t*)(dst))[1] = ((uint32_t)(a) >> 16) & 0xFF, \
+     ((uint8_t*)(dst))[2] = ((uint32_t)(a) >> 8) & 0xFF,  \
+     ((uint8_t*)(dst))[3] = ((uint32_t)(a) >> 0) & 0xFF)
 
-#define MEM_LOAD64BE(src)                        \
-    (((uint64_t)(((uint8_t *)(src))[0]) << 56) | \
-     ((uint64_t)(((uint8_t *)(src))[1]) << 48) | \
-     ((uint64_t)(((uint8_t *)(src))[2]) << 40) | \
-     ((uint64_t)(((uint8_t *)(src))[3]) << 32) | \
-     ((uint64_t)(((uint8_t *)(src))[4]) << 24) | \
-     ((uint64_t)(((uint8_t *)(src))[5]) << 16) | \
-     ((uint64_t)(((uint8_t *)(src))[6]) << 8) |  \
-     ((uint64_t)(((uint8_t *)(src))[7]) << 0))
+#define MEM_STORE64BE(dst, a)                             \
+    (((uint8_t*)(dst))[0] = ((uint64_t)(a) >> 56) & 0xFF, \
+     ((uint8_t*)(dst))[1] = ((uint64_t)(a) >> 48) & 0xFF, \
+     ((uint8_t*)(dst))[2] = ((uint64_t)(a) >> 40) & 0xFF, \
+     ((uint8_t*)(dst))[3] = ((uint64_t)(a) >> 32) & 0xFF, \
+     ((uint8_t*)(dst))[4] = ((uint64_t)(a) >> 24) & 0xFF, \
+     ((uint8_t*)(dst))[5] = ((uint64_t)(a) >> 16) & 0xFF, \
+     ((uint8_t*)(dst))[6] = ((uint64_t)(a) >> 8) & 0xFF,  \
+     ((uint8_t*)(dst))[7] = ((uint64_t)(a) >> 0) & 0xFF)
 
-#define MEM_STORE64BE(dst, a)                              \
-    (((uint8_t *)(dst))[0] = ((uint64_t)(a) >> 56) & 0xFF, \
-     ((uint8_t *)(dst))[1] = ((uint64_t)(a) >> 48) & 0xFF, \
-     ((uint8_t *)(dst))[2] = ((uint64_t)(a) >> 40) & 0xFF, \
-     ((uint8_t *)(dst))[3] = ((uint64_t)(a) >> 32) & 0xFF, \
-     ((uint8_t *)(dst))[4] = ((uint64_t)(a) >> 24) & 0xFF, \
-     ((uint8_t *)(dst))[5] = ((uint64_t)(a) >> 16) & 0xFF, \
-     ((uint8_t *)(dst))[6] = ((uint64_t)(a) >> 8) & 0xFF,  \
-     ((uint8_t *)(dst))[7] = ((uint64_t)(a) >> 0) & 0xFF)
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+// **************************************************
+// ************** SM3 CORE FUNCTIONS ****************
+// **************************************************
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
 
-static inline uint32_t rotl(uint32_t x, int s)
-{
-    return ((x) << (s) | (x) >> (32 - (s)));
-}
+#define rotl(x, n)    (((x) << (n)) | ((x) >> (32 - (n))))
+#define P0(x)         ((x) ^ rotl((x), 9) ^ rotl((x), 17))
+#define P1(x)         ((x) ^ rotl((x), 15) ^ rotl((x), 23))
+#define FF00(x, y, z) ((x) ^ (y) ^ (z))
+#define FF16(x, y, z) (((x) & (y)) | ((x) & (z)) | ((y) & (z)))
+#define GG00(x, y, z) ((x) ^ (y) ^ (z))
+#define GG16(x, y, z) ((((y) ^ (z)) & (x)) ^ (z))
 
-static inline uint32_t P0(uint32_t x)
-{
-    return x ^ rotl(x, 9) ^ rotl(x, 17);
-}
+#define R(A, B, C, D, E, F, G, H, xx)                                 \
+    do                                                                \
+    {                                                                 \
+        uint32_t SS1 = rotl((rotl(A, 12) + E + T[j]), 7);             \
+        uint32_t SS2 = SS1 ^ rotl(A, 12);                             \
+        uint32_t TT1 = FF##xx(A, B, C) + D + SS2 + (W[j] ^ W[j + 4]); \
+        uint32_t TT2 = GG##xx(E, F, G) + H + SS1 + W[j];              \
+        B            = rotl(B, 9);                                    \
+        D            = TT1;                                           \
+        F            = rotl(F, 19);                                   \
+        H            = P0(TT2);                                       \
+        j++;                                                          \
+    } while (0)
 
-static inline uint32_t P1(uint32_t x)
-{
-    return x ^ rotl(x, 15) ^ rotl(x, 23);
-}
+#define R4(A, B, C, D, E, F, G, H, xx) \
+    R(A, B, C, D, E, F, G, H, xx);     \
+    R(D, A, B, C, H, E, F, G, xx);     \
+    R(C, D, A, B, G, H, E, F, xx);     \
+    R(B, C, D, A, F, G, H, E, xx)
 
-static inline uint32_t FFj(uint32_t x, uint32_t y, uint32_t z, int j)
-{
-    if (j < 16)
-    {
-        return x ^ y ^ z;
-    }
-    else
-    {
-        return (x & y) | (x & z) | (y & z);
-    }
-}
-
-static inline uint32_t GGj(uint32_t x, uint32_t y, uint32_t z, int j)
-{
-    if (j < 16)
-    {
-        return x ^ y ^ z;
-    }
-    else
-    {
-        return (x & y) | (~x & z);
-    }
-}
-
-// T[j] = Tj <<< (j mod 32)
-static uint32_t T[64] = {
-    0x79cc4519, 0xf3988a32, 0xe7311465, 0xce6228cb, 0x9cc45197, 0x3988a32f,
-    0x7311465e, 0xe6228cbc, 0xcc451979, 0x988a32f3, 0x311465e7, 0x6228cbce,
-    0xc451979c, 0x88a32f39, 0x11465e73, 0x228cbce6, 0x9d8a7a87, 0x3b14f50f,
-    0x7629ea1e, 0xec53d43c, 0xd8a7a879, 0xb14f50f3, 0x629ea1e7, 0xc53d43ce,
-    0x8a7a879d, 0x14f50f3b, 0x29ea1e76, 0x53d43cec, 0xa7a879d8, 0x4f50f3b1,
-    0x9ea1e762, 0x3d43cec5, 0x7a879d8a, 0xf50f3b14, 0xea1e7629, 0xd43cec53,
-    0xa879d8a7, 0x50f3b14f, 0xa1e7629e, 0x43cec53d, 0x879d8a7a, 0x0f3b14f5,
-    0x1e7629ea, 0x3cec53d4, 0x79d8a7a8, 0xf3b14f50, 0xe7629ea1, 0xcec53d43,
-    0x9d8a7a87, 0x3b14f50f, 0x7629ea1e, 0xec53d43c, 0xd8a7a879, 0xb14f50f3,
-    0x629ea1e7, 0xc53d43ce, 0x8a7a879d, 0x14f50f3b, 0x29ea1e76, 0x53d43cec,
-    0xa7a879d8, 0x4f50f3b1, 0x9ea1e762, 0x3d43cec5,
+static const uint32_t T[64] = {
+    0x79cc4519U, 0xf3988a32U, 0xe7311465U, 0xce6228cbU, 0x9cc45197U,
+    0x3988a32fU, 0x7311465eU, 0xe6228cbcU, 0xcc451979U, 0x988a32f3U,
+    0x311465e7U, 0x6228cbceU, 0xc451979cU, 0x88a32f39U, 0x11465e73U,
+    0x228cbce6U, 0x9d8a7a87U, 0x3b14f50fU, 0x7629ea1eU, 0xec53d43cU,
+    0xd8a7a879U, 0xb14f50f3U, 0x629ea1e7U, 0xc53d43ceU, 0x8a7a879dU,
+    0x14f50f3bU, 0x29ea1e76U, 0x53d43cecU, 0xa7a879d8U, 0x4f50f3b1U,
+    0x9ea1e762U, 0x3d43cec5U, 0x7a879d8aU, 0xf50f3b14U, 0xea1e7629U,
+    0xd43cec53U, 0xa879d8a7U, 0x50f3b14fU, 0xa1e7629eU, 0x43cec53dU,
+    0x879d8a7aU, 0x0f3b14f5U, 0x1e7629eaU, 0x3cec53d4U, 0x79d8a7a8U,
+    0xf3b14f50U, 0xe7629ea1U, 0xcec53d43U, 0x9d8a7a87U, 0x3b14f50fU,
+    0x7629ea1eU, 0xec53d43cU, 0xd8a7a879U, 0xb14f50f3U, 0x629ea1e7U,
+    0xc53d43ceU, 0x8a7a879dU, 0x14f50f3bU, 0x29ea1e76U, 0x53d43cecU,
+    0xa7a879d8U, 0x4f50f3b1U, 0x9ea1e762U, 0x3d43cec5U,
 };
 
-static void sm3_fast_compress(uint32_t state[8], const uint8_t data[64])
+static void sm3_fast_compress(uint32_t digest[8], const uint8_t* data)
 {
-    // expand
-    uint32_t w[68];
-    for (int j = 0; j < 16; j++)
-    {
-        w[j] = MEM_LOAD32BE(data + 4 * j);
-    }
+    uint32_t A, B, C, D, E, F, G, H;
+    uint32_t W[68];
+
+    A = digest[0];
+    B = digest[1];
+    C = digest[2];
+    D = digest[3];
+    E = digest[4];
+    F = digest[5];
+    G = digest[6];
+    H = digest[7];
+
+    W[0]  = MEM_LOAD32BE(data + 0);
+    W[1]  = MEM_LOAD32BE(data + 4);
+    W[2]  = MEM_LOAD32BE(data + 8);
+    W[3]  = MEM_LOAD32BE(data + 12);
+    W[4]  = MEM_LOAD32BE(data + 16);
+    W[5]  = MEM_LOAD32BE(data + 20);
+    W[6]  = MEM_LOAD32BE(data + 24);
+    W[7]  = MEM_LOAD32BE(data + 28);
+    W[8]  = MEM_LOAD32BE(data + 32);
+    W[9]  = MEM_LOAD32BE(data + 36);
+    W[10] = MEM_LOAD32BE(data + 40);
+    W[11] = MEM_LOAD32BE(data + 44);
+    W[12] = MEM_LOAD32BE(data + 48);
+    W[13] = MEM_LOAD32BE(data + 52);
+    W[14] = MEM_LOAD32BE(data + 56);
+    W[15] = MEM_LOAD32BE(data + 60);
     for (int j = 16; j < 68; j++)
     {
-        w[j] = P1(w[j - 16] ^ w[j - 9] ^ rotl(w[j - 3], 15)) ^
-               rotl(w[j - 13], 7) ^ w[j - 6];
+        W[j] = P1(W[j - 16] ^ W[j - 9] ^ rotl(W[j - 3], 15)) ^
+               rotl(W[j - 13], 7) ^ W[j - 6];
     }
-    // compress
-    uint32_t A, B, C, D, E, F, G, H;
-    A = state[0], B = state[1], C = state[2], D = state[3];
-    E = state[4], F = state[5], G = state[6], H = state[7];
-    uint32_t SS1, SS2, TT1, TT2;
-    for (int j = 0; j < 64; j++)
-    {
-        SS1 = rotl((rotl(A, 12) + E + T[j]), 7);
-        SS2 = SS1 ^ rotl(A, 12);
-        TT1 = FFj(A, B, C, j) + D + SS2 + (w[j] ^ w[j + 4]);
-        TT2 = GGj(E, F, G, j) + H + SS1 + w[j];
-        D   = C;
-        C   = rotl(B, 9);
-        B   = A;
-        A   = TT1;
-        H   = G;
-        G   = rotl(F, 19);
-        F   = E;
-        E   = P0(TT2);
-    }
-    state[0] ^= A, state[1] ^= B, state[2] ^= C, state[3] ^= D;
-    state[4] ^= E, state[5] ^= F, state[6] ^= G, state[7] ^= H;
+
+    int j = 0;
+    R4(A, B, C, D, E, F, G, H, 00);
+    R4(A, B, C, D, E, F, G, H, 00);
+    R4(A, B, C, D, E, F, G, H, 00);
+    R4(A, B, C, D, E, F, G, H, 00);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    R4(A, B, C, D, E, F, G, H, 16);
+    digest[0] ^= A;
+    digest[1] ^= B;
+    digest[2] ^= C;
+    digest[3] ^= D;
+    digest[4] ^= E;
+    digest[5] ^= F;
+    digest[6] ^= G;
+    digest[7] ^= H;
 }
 
-static int uint64_add(uint64_t *self, uint64_t other)
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+// **************************************************
+// ************* SM3 CIPHER FUNCTION ****************
+// **************************************************
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+static int u64_add(uint64_t* r, uint64_t n)
 {
-    uint32_t a0, a1, b0, b1, r0, r1;
-    uint64_t t;
-    a0 = *self & UINT32_MAX, a1 = *self >> 32;
-    b0 = other & UINT32_MAX, b1 = other >> 32;
-
-    t  = (uint64_t)a0 + b0;
-    r0 = t & UINT32_MAX;
-    t  = t >> 32;
-
-    t  = (uint64_t)a1 + b1 + t;
-    r1 = t & UINT32_MAX;
-    t  = t >> 32;
-
-    *self = (uint64_t)r1 << 32 | r0;
-    return (int)t;
+    uint32_t a1  = (uint32_t)(*r >> 32);
+    uint32_t a0  = *r & UINT32_MAX;
+    uint32_t b1  = (uint32_t)(n >> 32);
+    uint32_t b0  = n & UINT32_MAX;
+    uint64_t tmp = (uint64_t)a0 + b0;
+    uint32_t r0  = tmp & UINT32_MAX;
+    tmp          = (uint64_t)a1 + b1 + (tmp >> 32);
+    uint32_t r1  = tmp & UINT32_MAX;
+    *r           = (uint64_t)r1 << 32 | r0;
+    return (int)(tmp >> 32); // carry bit
 }
 
-static void sm3_fast_init(Sm3CTX *ctx)
+static void sm3_fast_init(Sm3CTX* ctx)
 {
     static const uint32_t SM3_INIT_DIGEST[8] = {
-        0x7380166F,
-        0x4914B2B9,
-        0x172442D7,
-        0xDA8A0600,
-        0xA96F30BC,
-        0x163138AA,
-        0xE38DEE4D,
-        0xB0FB0E4E,
+        0x7380166F, 0x4914B2B9, 0x172442D7, 0xDA8A0600,
+        0xA96F30BC, 0x163138AA, 0xE38DEE4D, 0xB0FB0E4E,
     };
-    ctx->state[0] = SM3_INIT_DIGEST[0];
-    ctx->state[1] = SM3_INIT_DIGEST[1];
-    ctx->state[2] = SM3_INIT_DIGEST[2];
-    ctx->state[3] = SM3_INIT_DIGEST[3];
-    ctx->state[4] = SM3_INIT_DIGEST[4];
-    ctx->state[5] = SM3_INIT_DIGEST[5];
-    ctx->state[6] = SM3_INIT_DIGEST[6];
-    ctx->state[7] = SM3_INIT_DIGEST[7];
-
+    ctx->state[0]  = SM3_INIT_DIGEST[0];
+    ctx->state[1]  = SM3_INIT_DIGEST[1];
+    ctx->state[2]  = SM3_INIT_DIGEST[2];
+    ctx->state[3]  = SM3_INIT_DIGEST[3];
+    ctx->state[4]  = SM3_INIT_DIGEST[4];
+    ctx->state[5]  = SM3_INIT_DIGEST[5];
+    ctx->state[6]  = SM3_INIT_DIGEST[6];
+    ctx->state[7]  = SM3_INIT_DIGEST[7];
     ctx->data_bits = 0;
-    ctx->buf_size  = 0;
+
+    ctx->buf_size = 0;
 }
 
-static int sm3_fast_update(Sm3CTX *ctx, const uint8_t *in, size_t inl)
+static void sm3_fast_reset(Sm3CTX* ctx)
+{
+    sm3_fast_init(ctx);
+}
+
+static int sm3_fast_update(Sm3CTX* ctx, const uint8_t* in, size_t inl)
 {
     uint64_t inl_bits = (uint64_t)inl * 8;
-    if (inl_bits < inl || uint64_add(&(ctx->data_bits), inl_bits))
+    if (inl_bits < inl || u64_add(&(ctx->data_bits), inl_bits))
     {
         return -1; // input bits overflow
     }
@@ -219,8 +231,14 @@ static int sm3_fast_update(Sm3CTX *ctx, const uint8_t *in, size_t inl)
             sm3_fast_compress(ctx->state, in);
             in += 64, inl -= 64, block_num--;
         }
+        if (inl)
+        {
+            memcpy(ctx->buf, in, inl);
+            ctx->buf_size = inl;
+        }
+        return 0;
     }
-    while (inl)
+    if (inl)
     {
         size_t size = 64 - ctx->buf_size;
         if (size > inl)
@@ -236,10 +254,25 @@ static int sm3_fast_update(Sm3CTX *ctx, const uint8_t *in, size_t inl)
             ctx->buf_size = 0;
         }
     }
+    if (inl && ctx->buf_size == 0)
+    {
+        size_t block_num = inl / 64;
+        while (block_num)
+        {
+            sm3_fast_compress(ctx->state, in);
+            in += 64, inl -= 64, block_num--;
+        }
+        if (inl)
+        {
+            memcpy(ctx->buf, in, inl);
+            ctx->buf_size = inl;
+        }
+        return 0;
+    }
     return 0;
 }
 
-static void sm3_fast_final(Sm3CTX *ctx, uint8_t digest[32])
+static void sm3_fast_final(Sm3CTX* ctx, uint8_t digest[32])
 {
     size_t  pad_num = (64ULL + 56 - 1 - ctx->buf_size) % 64;
     uint8_t buf[64 * 2];
@@ -286,7 +319,7 @@ void Sm3Cipher::reset() noexcept
     sm3_fast_init(&(this->ctx));
 }
 
-void Sm3Cipher::update(const uint8_t *in, size_t inl)
+void Sm3Cipher::update(const uint8_t* in, size_t inl)
 {
     int err = sm3_fast_update(&(this->ctx), in, inl);
     if (err)
