8da56b86a6c66d468562be92a37d5d1fb0da7f51
oldprincess <zirui.gong@foxmail.com>
2023-12-12 23:33:53 +0800

    update sha1

--- gmlib/crypto/hash/sha1/sha1_standard.h
+++ gmlib/crypto/hash/sha1/sha1_standard.h
@@ -21,15 +21,61 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 OR OTHER DEALINGS IN THE SOFTWARE.
 */
-#ifndef _GMLIB_CRYPTO_HASH_SHA1_CIPHER_H
-#define _GMLIB_CRYPTO_HASH_SHA1_CIPHER_H
+#ifndef _GMLIB_CRYPTO_HASH_SHA1_STANDARD_H
+#define _GMLIB_CRYPTO_HASH_SHA1_STANDARD_H
 
-#include <gmlib/crypto/hash/sha1/sha1_standard.h>
+#include <TinyCrypto/hash/sha1/sha1_standard.h>
+#include <gmlib/exception.h>
 
 namespace gmlib {
 
-using Sha1Cipher = sha1_standard::Sha1Cipher;
+namespace sha1_standard {
+
+class Sha1Cipher
+{
+public:
+    static constexpr size_t BLOCK_SIZE  = SHA1_BLOCK_SIZE;
+    static constexpr size_t DIGEST_SIZE = SHA1_DIGEST_SIZE;
+
+private:
+    tc::Sha1StandardCTX ctx;
+
+public:
+    Sha1Cipher() noexcept
+    {
+        tc::sha1_standard_init(&this->ctx);
+    }
+
+public:
+    void reset() noexcept
+    {
+        tc::sha1_standard_reset(&this->ctx);
+    }
+
+    void update(const uint8_t* in, size_t inl) noexcept
+    {
+        if (tc::sha1_standard_update(&this->ctx, in, inl))
+        {
+            throw gmlib::Exception("sha1 input bits overflow");
+        }
+    }
+
+    void final(uint8_t digest[32]) noexcept
+    {
+        tc::sha1_standard_final(&this->ctx, digest);
+    }
+
+public:
+    static void compute(uint8_t digest[32], const uint8_t* msg, size_t msg_len)
+    {
+        Sha1Cipher h;
+        h.update(msg, msg_len);
+        h.final(digest);
+    }
+};
+
+} // namespace sha1_standard
 
 }; // namespace gmlib
 
-#endif // !_GMLIB_CRYPTO_HASH_sha1_CIPHER_H
+#endif // !_GMLIB_CRYPTO_HASH_SHA1_STANDARD_H
