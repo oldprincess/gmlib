442e788fedae5ab96ab57a0df836abd14269e48c
oldprincess <zirui.gong@foxmail.com>
2025-09-01 19:39:17 +0800

    fix(memxor): function undefined

--- include/gmlib/memory_utils/memxor.h
+++ include/gmlib/memory_utils/memxor.h
@@ -6,16 +6,16 @@
 
 namespace memory_utils {
 
+void memxor_n(void       *out,
+              const void *in1,
+              const void *in2,
+              std::size_t size) noexcept;
+
 template <std::size_t SIZE>
 static inline void memxor(void *out, const void *in1, const void *in2) noexcept
 {
     memxor_n(out, in1, in2, SIZE);
 }
-
-void memxor_n(void       *out,
-              const void *in1,
-              const void *in2,
-              std::size_t size) noexcept;
 
 } // namespace memory_utils
 
