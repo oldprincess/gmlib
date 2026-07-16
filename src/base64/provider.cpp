794b4559170f07b3eca1aeab56a4a6a1e82e29e4
oldprincess <zirui.gong@foxmail.com>
2026-07-16 23:03:22 +0800

    refactor(base64)

--- src/base64/provider.cpp
+++ src/base64/provider.cpp
@@ -1,6 +1,7 @@
 #include <gmlib/base64/provider.h>
 #include <gmlib/cpuinfo/cpuinfo.h>
 
+#include <cstddef>
 #include <cstring>
 
 #if defined(SUPPORT_BASE64_AVX2)
@@ -24,138 +25,105 @@ struct Provider
     Base64Provider fns;
 };
 
+static const Provider providers[] = {
 #if defined(SUPPORT_BASE64_AVX2)
-static const Provider base64_avx2_provider = {
-    []() noexcept -> bool { return cpuinfo::x86_64::cpu_supports_avx2(); },
     {
-        "avx2",
-        internal::avx2::base64_is_b64,
-        internal::avx2::base64_encode_outl,
-        internal::avx2::base64_encode_strl,
-        internal::avx2::base64_decode_outl,
-        internal::avx2::base64_encode,
-        internal::avx2::base64_decode,
+        []() noexcept -> bool { return cpuinfo::x86_64::cpu_supports_avx2(); },
+        {
+            "avx2",
+            internal::avx2::base64_is_b64,
+            internal::avx2::base64_encode_outl,
+            internal::avx2::base64_encode_strl,
+            internal::avx2::base64_decode_outl,
+            internal::avx2::base64_encode,
+            internal::avx2::base64_decode,
+        },
     },
-};
 #endif
 #if defined(SUPPORT_BASE64_CHROMIUM)
-static const Provider base64_chromium_provider = {
-    []() noexcept -> bool { return true; },
     {
-        "chromium",
-        internal::chromium::base64_is_b64,
-        internal::chromium::base64_encode_outl,
-        internal::chromium::base64_encode_strl,
-        internal::chromium::base64_decode_outl,
-        internal::chromium::base64_encode,
-        internal::chromium::base64_decode,
+        []() noexcept -> bool { return true; },
+        {
+            "chromium",
+            internal::chromium::base64_is_b64,
+            internal::chromium::base64_encode_outl,
+            internal::chromium::base64_encode_strl,
+            internal::chromium::base64_decode_outl,
+            internal::chromium::base64_encode,
+            internal::chromium::base64_decode,
+        },
     },
-};
 #endif
-static const Provider base64_common_provider = {
-    []() noexcept -> bool { return true; },
     {
-        "common",
-        internal::common::base64_is_b64,
-        internal::common::base64_encode_outl,
-        internal::common::base64_encode_strl,
-        internal::common::base64_decode_outl,
-        internal::common::base64_encode,
-        internal::common::base64_decode,
+        []() noexcept -> bool { return true; },
+        {
+            "common",
+            internal::common::base64_is_b64,
+            internal::common::base64_encode_outl,
+            internal::common::base64_encode_strl,
+            internal::common::base64_decode_outl,
+            internal::common::base64_encode,
+            internal::common::base64_decode,
+        },
     },
 };
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept
+{
+    return N;
+}
 
 const Base64Provider* get_base64_provider(const char* name) noexcept
 {
     if (name == nullptr)
     {
-        static const Base64Provider* default_provider =
-            []() noexcept -> const Base64Provider* {
-#if defined(SUPPORT_BASE64_AVX2)
-            if (base64_avx2_provider.available())
+        static const Base64Provider* default_provider = []() {
+            for (const Provider& provider : providers)
             {
-                return &base64_avx2_provider.fns;
+                if (provider.available())
+                {
+                    return &provider.fns;
+                }
             }
-#endif
-#if defined(SUPPORT_BASE64_CHROMIUM)
-            if (base64_chromium_provider.available())
-            {
-                return &base64_chromium_provider.fns;
-            }
-#endif
-            return &base64_common_provider.fns;
+            return static_cast<const Base64Provider*>(nullptr);
         }();
         return default_provider;
     }
     else
     {
-        if (std::strcmp(name, "avx2") == 0)
+        for (const Provider& provider : providers)
         {
-#if defined(SUPPORT_BASE64_AVX2)
-            if (base64_avx2_provider.available())
+            if (provider.available() &&
+                std::strcmp(provider.fns.algo_name, name) == 0)
             {
-                return &base64_avx2_provider.fns;
+                return &provider.fns;
             }
-#endif
-            return nullptr;
         }
-        else if (std::strcmp(name, "chromium") == 0)
-        {
-#if defined(SUPPORT_BASE64_CHROMIUM)
-            if (base64_chromium_provider.available())
-            {
-                return &base64_chromium_provider.fns;
-            }
-#endif
-            return nullptr;
-        }
-        else if (std::strcmp(name, "common") == 0)
-        {
-            return &base64_common_provider.fns;
-        }
-        else
-        {
-            return nullptr;
-        }
+        return nullptr;
     }
 }
 
-struct ProviderNameList
-{
-    const char* names[
-#if defined(SUPPORT_BASE64_AVX2)
-        1 + // avx2
-#endif
-#if defined(SUPPORT_BASE64_CHROMIUM)
-        1 + // chromium
-#endif
-        1 + // common
-        1   // nullptr
-    ];
-};
-
 const char* const* get_base64_supported_provider_names() noexcept
 {
-    static const ProviderNameList list = []() noexcept -> ProviderNameList {
+    static const auto provider_name_list = []() {
+        struct ProviderNameList
+        {
+            const char* names[array_size(providers) + 1];
+        };
         ProviderNameList list;
         int              idx = 0;
-#if defined(SUPPORT_BASE64_AVX2)
-        if (base64_avx2_provider.available())
+        for (const Provider& provider : providers)
         {
-            list.names[idx++] = "avx2";
+            if (provider.available())
+            {
+                list.names[idx++] = provider.fns.algo_name;
+            }
         }
-#endif
-#if defined(SUPPORT_BASE64_CHROMIUM)
-        if (base64_chromium_provider.available())
-        {
-            list.names[idx++] = "chromium";
-        }
-#endif
-        list.names[idx++] = "common";
-        list.names[idx]   = nullptr;
+        list.names[idx] = nullptr;
         return list;
     }();
-    return list.names;
+    return provider_name_list.names;
 }
 
 } // namespace base64
